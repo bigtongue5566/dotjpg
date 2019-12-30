2ec37802f88f06e4a1e27ab7d965246ed5c41f14
bigtongue5566 <bigtongue5566@gmail.com>
2019-12-30 15:28:44 +0800

    rm share button

--- config/passport.js
+++ config/passport.js
@@ -41,6 +41,7 @@ module.exports = function(passport) {
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true,
+    session: true
   },
   function(req, email, password, done) {
     User.findOne({ 'local.email':  email }, function(err, user) {
